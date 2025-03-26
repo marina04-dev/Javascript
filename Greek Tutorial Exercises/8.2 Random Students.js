61914c3d543150cfd7d6c8e109c91ec4ce3305cb
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-26 02:49:12 -0700

    Create 8.2 Random Students.js

---
+++ Greek Tutorial Exercises/8.2 Random Students.js
@@ -0,0 +1,64 @@
+//Lesson 8.2: Exercise 4- Exercise 5
+
+
+function Student(name, surname, grade) {
+  this.name = name;
+  this.surname = surname;
+  this.grade = grade;
+}
+
+
+Student.prototype.toString = function() {
+  return `${this.name} ${this.surname} (grade: ${this.grade})`;
+}
+
+
+let firstNames = ["Karen","Maria","Loren","Lucy","Tom","Timothee","Lara","Bob","John","Jonathan","Kylie","Julia","Peter","Hector","Jacob","Nick","Thomas","Marina","Kate","Anna"];
+let lastNames = ["Cruise","Holland","Pitt","Poole","Ashley","Jenner","Phelps","Kroft","Mask","Trump","Jenneris","Stew","Lopez","Affleck","Manoban","Osten","Einstein","Lancelot","Conor","Uster"];
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max-min) +min);
+}
+
+let students = new Array(10);
+for (let i=0; i<students.length; i++) {
+  students[i] = new Student(
+    firstNames[randomInt(0,firstNames.length)],
+    lastNames[randomInt(0,lastNames.length)]
+  );
+}
+
+console.log(students);
+
+function gradeStudent(student) {
+  student.grade = randomInt(0,11);
+}
+
+for (let student of students) {
+  gradeStudent(student);
+  console.log("" + student);
+}
+
+
+function average(students) {
+  let sum=0;
+  for (let student of students) {
+    sum += student.grade;
+  }
+  return sum/students.length;
+}
+console.log(`The Average is ${average(students)}`);
+
+let comparator = function(student1, student2) {
+  return student1.grade - student2.grade;
+}
+
+let comparatorDesc = function(student1, student2) {
+  return student2.grade - student1.grade;
+}
+
+students.sort(comparator);
+console.log(students);
+
+students.sort(comparatorDesc);
+console.log(students);
